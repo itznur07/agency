ef0cae5c46a2354696a115e5fda75cd64261fc50
Md Nuruddin <nuruddinalways99@gmail.com>
2023-02-05 07:37:40 +0600

    ft-sect

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import About from '../Components/About'
+import Summery from '../Components/SummeryBanner'
 
 const Home = () => {
   return (
     <div>
         <About />
+        <Summery />
     </div>
   )
 }
